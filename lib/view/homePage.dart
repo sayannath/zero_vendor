abcfc38302a2bed2bf5c937b84de31f8d5a7d36d
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-08-30 22:46:59 +0530

    homepage half

--- lib/view/homePage.dart
+++ lib/view/homePage.dart
@@ -12,15 +12,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: [
           Container(
-            padding: EdgeInsets.only(left: 16, top: 16,bottom: 16,right: 10),
+            padding: EdgeInsets.all(20),
             width: MediaQuery.of(context).size.width,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -36,7 +34,7 @@ class _HomePageState extends State<HomePage> {
                           TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      'Add / Delete/ Update Item',
+                      'May,12,2020',
                       style: TextStyle(
                         fontSize: 16,
                         color: Colors.grey[600],
@@ -45,7 +43,10 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
                 IconButton(
-                  icon: Icon(Icons.account_circle),
+                  icon: Icon(
+                    Icons.account_circle,
+                    size: 50,
+                  ),
                   onPressed: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
@@ -59,36 +60,119 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          Container(
-            height: 85,
-            padding: EdgeInsets.only(left:16,top: 16,bottom: 16),
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 10,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: EdgeInsets.only(right: 12),
-                  child: RaisedButton.icon(
-                    icon: Icon(Icons.child_care),
-                    label: Text('Item $index'),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    onPressed: () {},
-                    color: Colors.pink[100],
-                  ),
-                );
-              },
-            ),
+          display1(
+            context,
+            "Total sales",
+            "Rs 19,534",
+            Colors.cyan[400],
+            Colors.blue[400],
+            Colors.white,
           ),
-          Container(
-            child: Row(
-
+          display1(
+            context,
+            "Earnings in this\nmonth",
+            "Rs 1,534",
+            Colors.purple[50],
+            Colors.purple[100],
+            Colors.deepPurple[300],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue, width: 5),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 4),
+                          child: Text('Last 7 days'),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            // border: Border.all(color: Colors.blue, width: 5),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 4),
+                          child: Text('Last Month'),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
       ),
-
     );
   }
+}
+
+display1(BuildContext context, String title, String value, Color c1, Color c2,Color textc) {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      shadowColor: Colors.black,
+      elevation: 8,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [c1,c2],begin: Alignment.topLeft,end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        width: MediaQuery.of(context).size.width - 40,
+        height: 105,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: textc,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    value,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: textc,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    ),
+  );
 }
