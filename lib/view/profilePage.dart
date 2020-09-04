9d74bc48723393db61609b227317bf69127fbf59
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-09-05 01:02:31 +0530

    Ui changes

--- lib/view/profilePage.dart
+++ lib/view/profilePage.dart
@@ -49,114 +49,221 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            height: 500,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 499,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1386F0),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(0.0),
-                      bottomLeft: Radius.circular(27.0),
-                      bottomRight: Radius.circular(27.0),
-                      topRight: Radius.circular(0.0),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 500,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 499,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1386F0),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(0.0),
+                          bottomLeft: Radius.circular(27.0),
+                          bottomRight: Radius.circular(27.0),
+                          topRight: Radius.circular(0.0),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  child: CircleAvatar(
-                    radius: 80,
-                    backgroundColor: Color(0xFF1386F0),
-                    child: Opacity(
-                      opacity: 0.8,
+                    Container(
                       child: CircleAvatar(
                         radius: 80,
-                        backgroundImage: AssetImage('assets/images/avatar.png'),
+                        backgroundColor: Color(0xFF1386F0),
+                        child: Opacity(
+                          opacity: 0.8,
+                          child: CircleAvatar(
+                            radius: 80,
+                            backgroundImage: NetworkImage(
+                                'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Container(
-                  height: 150,
-                  width: 150,
-                  child: Container(
-                    alignment: Alignment.bottomRight,
-                    child: Icon(
-                      Icons.add_a_photo,
-                      size: 40,
-                      color: Colors.black54,
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: Container(
+                        alignment: Alignment.bottomRight,
+                        child: Icon(
+                          Icons.add_a_photo,
+                          size: 40,
+                          color: Colors.black54,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  height: 240,
-                  child: Text(
-                    'Remove',
-                    style: TextStyle(
-                        color: Colors.redAccent,
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  height: 345,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Shyam Singh',
-                        style: TextStyle(color: Colors.white, fontSize: 32),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      height: 225,
+                      child: Text(
+                        'Remove',
+                        style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(
-                        width: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      height: 335,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Shyam Singh',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontFamily: 'Montserrat',
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        ],
                       ),
-                      Icon(
-                        Icons.edit,
-                        color: Colors.white,
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      height: 390,
+                      child: Opacity(
+                        opacity: 0.7,
+                        child: Text(
+                          'Vendor',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  height: 395,
-                  child: Text(
-                    'Vendor',
-                    style: TextStyle(
-                        color: Colors.grey[400],
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          ListView(
-            children: [
-              ListTile(
-                leading: Icon(Icons.call),
-                title: Text(
-                  'Phone Number',
-                  style: TextStyle(
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.call,
                       color: Colors.black,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text('+ 91 0000000000'),
-                trailing: Icon(Icons.edit),
+                    ),
+                    title: Text(
+                      'Phone Number',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      '+ 91 0000000000',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                      ),
+                    ),
+                    trailing: Icon(
+                      Icons.edit,
+                      color: Colors.black,
+                      size: 22,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      'Email',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      'shyamsingh@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                      ),
+                    ),
+                    trailing: Icon(
+                      Icons.edit,
+                      color: Colors.black,
+                      size: 22,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.location_city,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      'Address',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      'Salt Lake, Kolkata',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                      ),
+                    ),
+                    trailing: Icon(
+                      Icons.edit,
+                      color: Colors.black,
+                      size: 22,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.map,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      'Pincode',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      '7000000',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                      ),
+                    ),
+                    trailing: Icon(
+                      Icons.edit,
+                      color: Colors.black,
+                      size: 22,
+                    ),
+                  ),
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
